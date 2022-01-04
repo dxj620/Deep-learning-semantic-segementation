2906e2a7dfdd6289abb1e8fdd3ef45ce3472ec06
platuo <49683490+dxj620@users.noreply.github.com>
2022-01-04 09:53:34 +0800

    Add files via upload

---
+++ High-Resolution-Remote-sensing-semantic-segmentation/models/deepLabv3plus/config.py
@@ -0,0 +1,82 @@
+# ----------------------------------------
+# Written by Yude Wang
+# ----------------------------------------
+import torch
+import argparse
+import os
+import sys
+import cv2
+import time
+
+
+class Configuration():
+	def __init__(self):
+		self.ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname("__file__"), '..', '..'))
+		self.EXP_NAME = 'deeplabv3+voc'
+
+		self.DATA_NAME = 'VOC2012'
+		self.DATA_AUG = True
+		self.DATA_WORKERS = 8
+		self.DATA_RESCALE = 512
+		self.DATA_RANDOMCROP = 512
+		self.DATA_RANDOMROTATION = 0
+		self.DATA_RANDOMSCALE = 2
+		self.DATA_RANDOM_H = 10
+		self.DATA_RANDOM_S = 10
+		self.DATA_RANDOM_V = 10
+		self.DATA_RANDOMFLIP = 0.5
+
+		self.MODEL_NAME = 'deeplabv3plus'
+		self.MODEL_BACKBONE = 'res50_atrous'
+		self.MODEL_OUTPUT_STRIDE = 16
+		self.MODEL_ASPP_OUTDIM = 256
+		self.MODEL_SHORTCUT_DIM = 48
+		self.MODEL_SHORTCUT_KERNEL = 1
+		self.MODEL_NUM_CLASSES = 6
+		self.MODEL_SAVE_DIR = os.path.join(self.ROOT_DIR, 'model', self.EXP_NAME)
+
+		self.TRAIN_LR = 0.007
+		self.TRAIN_LR_GAMMA = 0.1
+		self.TRAIN_MOMENTUM = 0.9
+		self.TRAIN_WEIGHT_DECAY = 0.00004
+		self.TRAIN_BN_MOM = 0.0003
+		self.TRAIN_POWER = 0.9
+		self.TRAIN_GPUS = 4
+		self.TRAIN_BATCHES = 16
+		self.TRAIN_SHUFFLE = True
+		self.TRAIN_MINEPOCH = 0
+		self.TRAIN_EPOCHS = 46
+		self.TRAIN_LOSS_LAMBDA = 0
+		self.TRAIN_TBLOG = True
+		self.TRAIN_CKPT = None  # os.path.join(self.ROOT_DIR,'model/deeplabv3+voc/deeplabv3plus_xception_VOC2012_itr0.pth')
+
+		self.LOG_DIR = os.path.join(self.ROOT_DIR, 'log', self.EXP_NAME)
+
+		self.TEST_MULTISCALE = [0.5, 0.75, 1.0, 1.25, 1.5, 1.75]
+		self.TEST_FLIP = True
+		self.TEST_CKPT = os.path.join(self.ROOT_DIR,
+									  'model/deeplabv3+voc/deeplabv3plus_res101_atrous_VOC2012_epoch46_all.pth')
+		self.TEST_GPUS = 4
+		self.TEST_BATCHES = 16
+
+		self.__check()
+		self.__add_path(os.path.join(self.ROOT_DIR, 'lib'))
+
+	def __check(self):
+		if not torch.cuda.is_available():
+			raise ValueError('config.py: cuda is not avalable')
+		if self.TRAIN_GPUS == 0:
+			raise ValueError('config.py: the number of GPU is 0')
+		# if self.TRAIN_GPUS != torch.cuda.device_count():
+		#	raise ValueError('config.py: GPU number is not matched')
+		if not os.path.isdir(self.LOG_DIR):
+			os.makedirs(self.LOG_DIR)
+		if not os.path.isdir(self.MODEL_SAVE_DIR):
+			os.makedirs(self.MODEL_SAVE_DIR)
+
+	def __add_path(self, path):
+		if path not in sys.path:
+			sys.path.insert(0, path)
+
+
+cfg = Configuration()
